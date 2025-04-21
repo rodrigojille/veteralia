4dcd114154c6442714236d7f724c45240c2ec5ed
rodrigojille <128759055+rodrigojille@users.noreply.github.com>
2025-04-21 09:24:07 -0600

    Fix Netlify build: add leaflet/@types/leaflet and set leaflet as external in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,4 +3,12 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   reactStrictMode: true,
   i18n,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = { fs: false, module: false };
+    }
+    config.externals = config.externals || [];
+    config.externals.push('leaflet');
+    return config;
+  },
 };
