320ce8789224319adc07c9047de65f63211e887c
rodrigojille <128759055+rodrigojille@users.noreply.github.com>
2025-04-22 14:03:25 -0600

    Fix CORS on Heroku by setting trust proxy in NestJS main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
   app.enableCors({
     origin: [
       'https://vetoralia.com',
