65f343cf1d9f61685cdc9276cd7f1d9f7b20d000
rodrigojille <128759055+rodrigojille@users.noreply.github.com>
2025-04-23 10:25:42 -0600

    chore: auto migration and code changes [1745403938.79205]

---
+++ backend/src/migrations/1745425539789-AutoMig_1745403938.79205.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AutoMig1745403938.792051745425539789 implements MigrationInterface {
+    name = 'AutoMig1745403938.792051745425539789'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "vet_availability" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "vetId" uuid NOT NULL, "dayOfWeek" integer, "date" date, "startTime" character varying(5) NOT NULL, "endTime" character varying(5) NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_5ba322395ca03ead268db01bee0" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "vet_availability" ADD CONSTRAINT "FK_0ab9403178a33a0d90ac60d625e" FOREIGN KEY ("vetId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "vet_availability" DROP CONSTRAINT "FK_0ab9403178a33a0d90ac60d625e"`);
+        await queryRunner.query(`DROP TABLE "vet_availability"`);
+    }
+
+}
